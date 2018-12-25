379644f620262fce8f1294aa721e7277b43f68e8
BrianArchibald <brianallenarchibald@gmail.com>
2018-12-25 14:02:38 -0600

    getting input and storing in react, use of this

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,23 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-	onInputChange(event) {
-		console.log(event.target.value);
+	// onInputChange(event) {
+	// 	console.log(event.target.value);
+	// }
+
+	state = {term: ''};
+
+	onFormSubmit = (event) => {
+		event.preventDefault();
 	}
 
 	render () {
 		return (
 			<div className="ui segment">
-				<form className="ui form">
+				<form onSubmit={this.onFormSubmit} className="ui form">
 					<div className="field">
 						<label>Image Search</label>
-						<input type="text" onChange={this.onInputChange}/> 
+						<input type="text" value ={this.state.term} onChange={(e) => this.setState({ term: e.target.value})}/> 
 					</div>
 				</form>
 			</div>
@@ -20,6 +26,11 @@ class SearchBar extends React.Component {
 }
 // I dont want to call the onInputChange with () on  the input as it would call on render, common error
 //  There is also onClick and onSubmit
+
+// Alternative way to call onInputChange, use when only doing a single event
+// <input type="text" onChange={(event) => console.log(event.target.value)}/> 
+// Other way 
+// <input type="text" onChange={this.onInputChange}/>
 
 
 export default SearchBar;
